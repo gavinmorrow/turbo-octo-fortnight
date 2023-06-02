15429e88c0803290804d1b7e2d270593a81813a0
Gavin Morrow <gavinfmorrow@gmail.com>
2023-06-01 20:38:53 -0400

    Get hello world example running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+const ROOT_PATH: &str = "localhost:7878";
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&ROOT_PATH.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
